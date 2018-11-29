8d3463d6ce480f05fad2566821ebd6e8208888bb
Yong Kang <yong.kang@oakridge.io>
2018-11-29 22:15:16 +0000

    Update production.cfg.py

--- production.cfg.py
+++ production.cfg.py
@@ -1,6 +1,8 @@
 import multiprocessing
 
 bind = "0.0.0.0:2018"
+certfile = "/etc/letsencrypt/live/cloud.oakridge.vip/fullchain.pem"
+keyfile = "/etc/letsencrypt/live/cloud.oakridge.vip/privkey.pem"
 workers = multiprocessing.cpu_count() * 2 + 1
 daemon = True
 pidfile = '/tmp/cloud_api.pid'
